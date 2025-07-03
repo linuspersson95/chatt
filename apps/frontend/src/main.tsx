ebf763ecd26977180325f291db70f6244e04f4a0
Linus Persson <linuspersson95@gmail.com>
2025-07-03 16:22:19 +0200

    Add tamagui plugin

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,13 +1,25 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { TamaguiProvider, createTamagui } from '@tamagui/core';
+import { defaultConfig } from '@tamagui/config/v4';
 import App from './app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const config = createTamagui(defaultConfig);
+
+type Config = typeof config;
+
+declare module '@tamagui/core' {
+  interface TamaguiCustomConfig extends Config {}
+}
+
 root.render(
   <StrictMode>
-    <App />
+    <TamaguiProvider config={config}>
+      <App />
+    </TamaguiProvider>
   </StrictMode>
 );
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types='vitest' />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { tamaguiPlugin } from '@tamagui/vite-plugin';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
